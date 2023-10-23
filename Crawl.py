47c10598900b5af8002a7814fcdbf9c294e0e02b
dunp7 <dam0709123@gmail.com>
2023-10-23 21:54:37 +0700

    modify crawl

--- Crawl.py
+++ Crawl.py
@@ -1,6 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # set driver
 driver = webdriver.Chrome()
-
+# access website baomoi.com
 driver.get('https://baomoi.com')
+
+data = driver.find_elements(By.XPATH, '//*[@id="__next"]/div[3]/div/div')
+for i in data:
+    print(i.text)
+driver.quit()
