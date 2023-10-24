28a8169c0df910eb26297215982f130489ae848e
dunp7 <dam0709123@gmail.com>
2023-10-24 22:25:01 +0700

    modify

--- Crawl.py
+++ Crawl.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 # set driver
 driver = webdriver.Chrome()
@@ -8,33 +9,48 @@ driver = webdriver.Chrome()
 driver.get('https://oto.com.vn')
 
 ### Find product
-car = ['as']
+brand_car = ['Porsche']
 
 # Find a box that need to find the product
 search_email = driver.find_element(By.ID,"txtKeyword")
 search_email.send_keys('Porsche')
 search_email.send_keys(Keys.RETURN)
-#
-# js_path = 'document.querySelector("#box-list-car > div.wrap-load-more.set-relative > span")'
 
-# nut_hien_thi_them = driver.find_element(By.CLASS_NAME,"btn-loadmore")
-# driver.execute_script(js_path,nut_hien_thi_them)
-# nut_hien_thi_them.click()
 
-detail_car = driver.find_element(By.CSS_SELECTOR,'a[title="Cần bán Porsche Cayenne Turbo 2018 - Chạy hơn 1 vạn như mới"]').click()
+while True:
+    try:
+        # Find the "Hiển thị thêm" button
+        nut_hien_thi_them = driver.find_element(By.CLASS_NAME, "btn-loadmore")
+        
+        if nut_hien_thi_them.is_enabled():
+            # Scroll to the element to make it visible
+            ActionChains(driver).move_to_element(nut_hien_thi_them).perform()
+            
+            # Click the button
+            nut_hien_thi_them.click()
+        else:
+            # If the button is no longer enabled, break out of the loop
+            break
+    except Exception as e:
+        # Handle exceptions if necessary
+        print(f"An error occurred: {str(e)}")
 
-driver.back()
-# car_info = driver.find_elements(By.CLASS_NAME,'box-list-car')
-# for i in car_info:
-#     print(i.text)
+
+
 parent_element = driver.find_element(By.CLASS_NAME,"box-list-car")
-elements = parent_element.find_elements(By.CSS_SELECTOR,'a[class][title]')
-# Extract and print the titles
-titles = [element.get_attribute("title") for element in elements]
-for title in titles:
-    print(title)
+elements = parent_element.find_elements(By.CSS_SELECTOR,'a[href][class][title]')
+# Extract the href and access to each href
+
+href_links = [element.get_attribute("href") for element in elements]
+
+
+for href_link in href_links:
+    print(href_link)
+    driver.get(href_link)
+    time.sleep(1)
 
 time.sleep(5)
+
 
 
 # Turn off the website
