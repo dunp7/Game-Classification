6a2fd194fdb6ad0ef5cf698a4c9d561e46109344
dunp7 <dam0709123@gmail.com>
2023-10-29 14:34:16 +0700

    change

--- Crawling/otocom_crawl/Crawl.py
+++ Crawling/otocom_crawl/Crawl.py
@@ -1,5 +1,4 @@
-from selenium import webdriver
-from Crawling.crawl_tool import Crawling_tool
+from Crawl_tool_base import Crawl_tool
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
@@ -7,35 +6,26 @@ from selenium.common.exceptions import NoSuchElementException
 import time
 import csv
 
-# create link_car.csv
-link_car = 'all_link_cars.csv'
-with open(file_path, 'w', newline='') as csvfile:
-    csv_writer = csv.writer(csvfile)
-    csv_writer.writerow([])
-
-# link
 link = 'https://oto.com.vn'
 
-class Crawling_link_tool(Crawling_tool):
+# set driver
+
+class Crawling_tool(Crawl_tool):
     def __init__(self):
         #initial set up
-        self.driver = webdriver.Chrome()
-
-    def access(self,link):
-        # Access the website
-        self.driver.get('https://oto.com.vn')
-
-    def quit(self):
-        # Turn off the website
-        self.driver.quit()
+        super().__init__()
 
     def input_the_brand_car(self,brand_name):
         search_box = self.driver.find_element(By.ID,"txtKeyword")
         search_box.send_keys(brand_name)
         search_box.send_keys(Keys.RETURN)
 
-    def loading_all_the_available_car(self):
-        # scroll all the car on the page (limit 20 times)
+    def scroll_to_the_info_box(self):
+        key = self.driver.find_element(By.CLASS_NAME,'box-info-detail')
+        ActionChains(self.driver).move_to_element(key).perform()  
+
+    def loading_all_the_car(self):
+        # scroll all the car on the page
         for count in range(20):
             try:
                 # Find the "Hiển thị thêm" button
@@ -66,22 +56,80 @@ class Crawling_link_tool(Crawling_tool):
         href_links = [element.get_attribute("href") for element in elements]
         return href_links
 
-    def save_link_into_csvfile(self,csv_file,link,brand):
+    def add_information_into_csvfile(self,csv_file,data,brand):
         ## Read into file csv
         with open(csv_file, mode='a', newline='',encoding='utf-8') as file:
             writer = csv.writer(file)
             # Write the data
-            writer.writerows(link)
-        print(f'link {brand} has been written to {csv_file}')
-    
-    def crawl_link_cars(csv_file,link,brand):
-        self.access
+            rows = zip(*data.values())  # Transpose the data to align rows correctly
+            writer.writerows(rows)
+        print(f'{brand} has been written to {csv_file}')
 
-class Crawling_features_tool:
-    def __init__(self):
-        pass
+    def create_features(self):
+        return { 'brand': [],'name': [],'price' : [],'nam_sx' : [],'origin' : [],\
+                'type_car' : [],'km_traveled' : [],'gear' : [], 'condition' :[],'fuel' : []}
 
 
+    def crawling_data(self,brand_name,csv_file,link):
+        data = self.create_features()
+        self.access(link)
+        self.input_the_brand_car(brand_name)
+        self.loading_all_the_car()
+        href_links = self.find_all_link_cars()
+        #take all data for each features
+        for href_link in href_links:
+            try:
+                self.driver.get(href_link)
+                self.driver.implicitly_wait(10)  # Wait for elements to be found within a 10-second window
+                self.scroll_to_the_info_box()
+                # Continue processing for this href_link
+            except:
+                print(f" for {href_link} :Error")
+                # Handle the timeout for this specific link 
+                continue  # Move to the next href_link after handling the exception
+            # get brand
+            data['brand'].append(brand_name)
 
-if __name__ == 'main':
+            # get name
+            try:
+                data['name'].append(self.driver.find_element(By.CLASS_NAME,'title-detail').text)
+            except:
+                print("Error in get name")
+                break
+
+            # get price
+            try:
+                data['price'].append(self.driver.find_element(By.CLASS_NAME,'price').text)
+            except:
+                print("Error in get price")
+                break
+
+            # get all the information from the box which class is list-info
+            try: 
+                info_list = self.driver.find_element(By.CLASS_NAME,'list-info')
+                elements = info_list.find_elements(By.CSS_SELECTOR,"li")
+                for element in elements:
+                    label_text = element.find_element(By.CSS_SELECTOR, 'label.label').text
+                    if "Năm SX" in label_text:
+                        data['nam_sx'].append(element.text.replace('\n', ' '))
+                    if "Kiểu dáng" in label_text:
+                        data['type_car'].append(element.text.replace('\n', ' '))
+                    if "Tình trạng" in label_text:
+                        data['condition'].append(element.text.replace('\n', ' '))
+                    if "Xuất xứ" in label_text:
+                        data['origin'].append(element.text.replace('\n', ' '))
+                    if  "Km đã đi" in label_text:
+                        data['km_traveled'].append(element.text.replace('\n', ' '))
+                    if "Hộp số" in label_text:
+                        data['gear'].append(element.text.replace('\n', ' '))
+                    if "Nhiên liệu" in label_text:
+                        data['fuel'].append(element.text.replace('\n', ' '))
+                
+            except:
+                print("Error in get info_list")
+            time.sleep(3)
+        self.quit()
+        self.add_information_into_csvfile(csv_file,data,brand_name)
+
+if __name__ == "__main__":
     pass
